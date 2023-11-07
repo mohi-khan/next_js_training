3437db8fb2701870ca014e55d515d569d7f92616
mohi-khan <mohi.khan@gmail.com>
2023-11-07 06:54:32 +0600

    First Commit

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,3 @@
+export default function page(){
+    return <h1  className="text-blue-500">this is invoice pages</h1>
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,6 @@
+import { lusitana } from "../ui/fonts";
+export default function page()
+{
+    return <h1 className={'${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal'}>This is Dashboard</h1>;
+   
+}
